1c4f4ce0ccd8464127b7224d57ff1075509ed59c
Lifka <javierizquierdovera@gmail.com>
2020-06-22 18:34:16 -0400

    Improve upload performance and handle errors

--- app/index.py
+++ app/index.py
@@ -16,6 +16,7 @@ VAULT = "vault"
 
 app = Flask(__name__)
 app.config["VAULT"] = VAULT
+app.config["files_by_hash"] = {}
 
 @app.route('/')
 def home():
@@ -28,18 +29,18 @@ def about():
 @app.route('/upload-file', methods=["POST"])
 def upload_file():
     if request.method != 'POST':
-        return;
+        return json.dumps({"result": "error", "message": "No POST request"})
 
     if 'file' not in request.files:
-        print("[upload_file] Upload error")
+        return json.dumps({"result": "error", "message": "Upload error"})
 
     file = request.files["file"] 
     if not file.filename:
-        print("[upload_file] Empty file")
+        return json.dumps({"result": "error", "message": "Empty file"})
 
     file_path = save_file(file, app.config["VAULT"])
     print("[upload_file] Sent file -> {}: {}".format(file_path, file))
-    return json.dumps({"result": "OK", "hash":'{}'.format(md5(file_path))})
+    return json.dumps({"result": "success", "hash":'{}'.format(md5(file_path))})
 
 @app.route('/file-analysis/<hash>')
 def file_analysis(hash):
@@ -48,7 +49,7 @@ def file_analysis(hash):
         print("[file_analysis] No file(s) found for hash -> {}".format(hash))
         return home()
     print("[file_analysis] Get results from -> {}".format(files))
-    app.config["current_file"] = files[0]
+    app.config["files_by_hash"][hash] = files[0]
     return render_template("scan-results.html")
 
 def async_action(f):
@@ -60,7 +61,10 @@ def async_action(f):
 @app.route('/file-analysis/<hash>/result', methods=["POST"])
 @async_action
 async def file_analysis_result(hash):
-    result = scan_file(app.config["current_file"])
+    if hash not in app.config["files_by_hash"]:
+        app.config["files_by_hash"][hash] = get_file_by_hash(hash)
+    print('file_analysis_result from -> {}'.format(app.config["files_by_hash"][hash]))
+    result = scan_file(app.config["files_by_hash"][hash])
 
     return result
 
