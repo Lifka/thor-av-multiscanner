db0d5e1dbce3462d88975208756ca7b0296bee5f
Lifka <javierizquierdovera@gmail.com>
2020-06-28 03:41:43 -0400

    Add imports to PE analyser

--- thor/core/PE_analyser.py
+++ thor/core/PE_analyser.py
@@ -4,18 +4,14 @@
 # Created by Javier Izquierdo Vera <javierizquierdovera.com>.
 # This program is free software, you can redistribute it and/or modify it under the terms of GPLv2.
 
-import json
-import pefile
-import datetime
+import json, pefile, datetime, ordlookup
 
 def get_section_name(section_raw_name):
     try:
         section_name = str(section_raw_name, 'utf-8').encode('ascii', errors='ignore').strip().decode('ascii').strip(' \t\r\n\0')
     except:
         section_name = str(section_raw_name, 'ISO-8859-1').encode('ascii', errors='ignore').strip().decode('ascii').strip(' \t\r\n\0')
-    if section_name == '':
-        section_name = '.noname'
-    return section_name
+    return section_name if section_name != '' else '.noname'
 
 def get_section_data(section):
     section_data = {}
@@ -37,6 +33,24 @@ MACHINE_CODES = {
 def get_target_machine(pe):
     return '' if not pe.FILE_HEADER.Machine in MACHINE_CODES else MACHINE_CODES[pe.FILE_HEADER.Machine]
 
+def get_imports_info(pe):
+    if not hasattr(pe, 'DIRECTORY_ENTRY_IMPORT'):
+        return []
+    imports = []
+    for entry in pe.DIRECTORY_ENTRY_IMPORT:
+        import_info = {}
+        libname = entry.dll.decode().lower() if isinstance(entry.dll, bytes) else entry.dll.lower()
+        import_info['Library'], import_info['Functions'] = libname, []
+        for imp in entry.imports:
+            funcname = ordlookup.ordLookup(entry.dll.lower(), imp.ordinal, make_name=True) if not imp.name else imp.name
+            if not funcname:
+                continue
+            if isinstance(funcname, bytes):
+                funcname = funcname.decode()
+            import_info['Functions'].append({'Address' : imp.address, 'Name' : funcname, 'Ordinal' : imp.ordinal if imp.ordinal else ''})
+            imports.append(import_info)
+    return imports
+
 def get_compilation_timestamp(pe):
     return datetime.datetime.fromtimestamp(pe.FILE_HEADER.TimeDateStamp).strftime('%Y-%m-%d %H:%M:%S')
 
@@ -44,12 +58,12 @@ def get_sections(file_path, pe):
     return { get_section_name(section.Name):get_section_data(section) for section in pe.sections }
 
 def get_pe_info_json(file_path):
-    sections, entry_point, target_machine, compilation_timestamp = get_pe_info(file_path)
-    return json.dumps({ "sections": sections, "entry_point": entry_point, "target_machine": target_machine, "compilation_timestamp": compilation_timestamp})
+    sections, entry_point, target_machine, compilation_timestamp, imports = get_pe_info(file_path)
+    return json.dumps({ "sections": sections, "entry_point": entry_point, "target_machine": target_machine, "compilation_timestamp": compilation_timestamp, "imports": imports})
  
 def get_pe_info(file_path):
     try:
         pe = pefile.PE(file_path)
     except Exception as e:
         return {} # DOS Header magic not found.
-    return get_sections(file_path, pe), get_entry_point(pe), get_target_machine(pe), get_compilation_timestamp(pe)
+    return get_sections(file_path, pe), get_entry_point(pe), get_target_machine(pe), get_compilation_timestamp(pe), get_imports_info(pe)
